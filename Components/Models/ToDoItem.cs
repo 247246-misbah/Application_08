bfc9a9a1a1108656553473f396f9c3efa547bfa1
247246-misbah <247246@students.au.edu.pk>
2026-05-25 23:59:19 +0500

    first commit

---
+++ Components/Models/ToDoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoListApp.Models
+{
+    public class ToDoItem
+    {
+        public string Title { get; set; } = "";
+        public bool IsDone { get; set; }
+        public bool IsEditing { get; set; }
+    }
+}
